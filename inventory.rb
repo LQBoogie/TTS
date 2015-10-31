a3f30a57e1d42d6ee655717da349c9b1679fd0fc
btyy77c <btyy77c@yahoo.com>
2015-10-31 08:06:59 -0400

    Added instructions

---
+++ inventory.rb
@@ -0,0 +1,13 @@
+
+#Inventory from Laquana and Emily
+
+#TODO: Make an initial inventory and print out all items
+
+#TODO: While loop that promts user for 1) "update" 2) "add" new item or 3) "quit"
+
+   #TODO: if "update" - update inventory items, reprint list, return to prompt
+
+   #TODO: if "add" - add new item to list, reprint list, return to prompt
+
+   #TODO: if "quit" - exit loop
+
